ddedce8810f6d7ce0e7315ff794de4387d0fe959
yonggyo1125 <yonggyo00@kakao.com>
2025-10-28 10:43:32 +0900

    .

--- src/main/java/com/codefactory/delivery/global/infrastructure/event/EventConfig.java
+++ src/main/java/com/codefactory/delivery/global/infrastructure/event/EventConfig.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @Configuration
 public class EventConfig {
     @Autowired
--- src/main/java/com/codefactory/delivery/payment/domain/service/OrderAcceptEventHandler.java
+++ src/main/java/com/codefactory/delivery/payment/domain/service/OrderAcceptEventHandler.java
@@ -1,12 +1,14 @@
 package com.codefactory.delivery.payment.domain.service;
 
 import com.codefactory.delivery.order.domain.event.OrderAcceptEvent;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Service
 public class OrderAcceptEventHandler {
 
+    @Async
     @TransactionalEventListener(OrderAcceptEvent.class)
     public void handle(OrderAcceptEvent event) {
         // 주문 접수 후 결제 진행 도메인 로직 추가
