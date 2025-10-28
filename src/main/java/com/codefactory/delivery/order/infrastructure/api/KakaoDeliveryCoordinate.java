b7619b64ce213a3adc1318e513a2bb11870d28af
yonggyo1125 <yonggyo00@kakao.com>
2025-10-28 10:58:43 +0900

    .

--- src/main/java/com/codefactory/delivery/order/infrastructure/api/KakaoDeliveryCoordinate.java
+++ src/main/java/com/codefactory/delivery/order/infrastructure/api/KakaoDeliveryCoordinate.java
@@ -1,9 +1,11 @@
 package com.codefactory.delivery.order.infrastructure.api;
 
 import com.codefactory.delivery.order.domain.service.DeliveryCoordinate;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class KakaoDeliveryCoordinate implements DeliveryCoordinate {
     @Override
     public List<Double> getCoordinate(String address) {
