923851ff1ccafc607e46fdd31d7bb19049a25b02
yonggyo1125 <yonggyo00@kakao.com>
2025-10-30 23:10:14 +0900

    .

--- src/main/java/com/codefactory/delivery/order/domain/cart/Cart.java
+++ src/main/java/com/codefactory/delivery/order/domain/cart/Cart.java
@@ -6,6 +6,7 @@ import com.codefactory.delivery.user.domain.UserId;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -15,7 +16,7 @@ import java.util.Objects;
 @Entity
 @Access(AccessType.FIELD)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(EntityListeners.class)
+@EntityListeners(AuditingEntityListener.class)
 public class Cart {
     @EmbeddedId
     @GeneratedValue(strategy = GenerationType.UUID)
