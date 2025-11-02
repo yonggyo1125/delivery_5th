db7b60638861327e921e3eb91fcfc5643e85c6df
yonggyo1125 <yonggyo00@kakao.com>
2025-11-03 08:09:48 +0900

    .

--- src/test/java/com/codefactory/delivery/user/test/MockUser.java
+++ src/test/java/com/codefactory/delivery/user/test/MockUser.java
@@ -5,6 +5,7 @@ import org.springframework.security.test.context.support.WithSecurityContext;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
