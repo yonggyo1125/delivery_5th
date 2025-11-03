ce06b3003161c88d413b3fb6c57c53e2e4e26d95
yonggyo1125 <yonggyo00@kakao.com>
2025-11-03 23:08:16 +0900

    상품 등록, 삭제 기능 구현 중

--- src/test/java/com/codefactory/delivery/item/application/service/ItemServiceTest.java
+++ src/test/java/com/codefactory/delivery/item/application/service/ItemServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,6 +16,7 @@ import java.util.stream.IntStream;
 
 @SpringBootTest
 @DisplayName("메뉴 기능 테스트")
+@ActiveProfiles("test")
 public class ItemServiceTest {
 
     @Autowired
